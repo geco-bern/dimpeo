f03045573711a981524b334c891acc31cc7cf3d8
brdav <david.bruggemann@datascience.ch>
2024-08-09 15:47:27 +0200

    update nn

--- tools/train_double_logistic.py
+++ tools/train_double_logistic.py
@@ -1,6 +1,7 @@
 import numpy as np
 import h5py
 import os
+import argparse
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
@@ -11,44 +12,75 @@ from sklearn.metrics import d2_pinball_score
 from model.mlp import MLP
 
 
-PATH = "/data_1/scratch_1/processed/nn_dataset.h5"
+PATH = "/data_1/scratch_1/processed/nn_dataset_v2.h5"
 YEARS_IN_TRAIN = 6  # first six years in train, last year in test
 SPLIT_IDX = 73 * YEARS_IN_TRAIN
 
 
 class H5Dataset():
-    def __init__(self, file_path):
+
+    all_features = ["lon", "lat", "dem", "fc", "fh", "slope", "easting", "northing", "twi", "rugg", "curv"]
+
+    def __init__(self, file_path, features=None):
         self.file_path = file_path
+        self.features = features if features is not None else self.all_features
         with h5py.File(self.file_path, "r") as file:
             self.dataset_len = len(file.get("doy"))
             self.ndvi_ds = file.get("ndvi")[:]
             self.doy_ds = file.get("doy")[:].astype(int)
-            self.lon_lat_ds = file.get("lon_lat")[:]
-            self.dem_ds = file.get("dem")[:]
-            self.fc_ds = file.get("fc")[:]
-            self.fc_ds[self.fc_ds == -9999] = np.nan
-            self.fh_ds = file.get("fh")[:]
-            self.slope_ds = file.get("slope")[:]
-            self.easting_ds = file.get("easting")[:]
-            self.northing_ds = file.get("northing")[:]
-            self.twi_ds = file.get("twi")[:]
+            if "lon" in self.features or "lat" in self.features:
+                self.lon_lat_ds = file.get("lon_lat")[:]
+            if "dem" in self.features:
+                self.dem_ds = file.get("dem")[:]
+            if "fc" in self.features:
+                self.fc_ds = file.get("fc")[:]
+                self.fc_ds[self.fc_ds == -9999] = np.nan
+            if "fh" in self.features:
+                self.fh_ds = file.get("fh")[:]
+            if "slope" in self.features:
+                self.slope_ds = file.get("slope")[:]
+            if "easting" in self.features:
+                self.easting_ds = file.get("easting")[:]
+            if "northing" in self.features:
+                self.northing_ds = file.get("northing")[:]
+            if "twi" in self.features:
+                self.twi_ds = file.get("twi")[:]
+            if "rugg" in self.features:
+                self.rugg_ds = file.get("rugg")[:]
+            if "curv" in self.features:
+                self.curv_ds = file.get("curv")[:]
 
         self.missingness = np.load(os.path.join(os.path.dirname(file_path), "missingness.npy"))
 
     def __getitem__(self, index):
         ndvi = self.ndvi_ds[index]
         doy = self.doy_ds[index]
-        lon_lat = self.lon_lat_ds[index]
-        dem = self.dem_ds[index]
-        fc = self.fc_ds[index]
-        fh = self.fh_ds[index]
-        slope = self.slope_ds[index]
-        easting = self.easting_ds[index]
-        northing = self.northing_ds[index]
-        twi = self.twi_ds[index]
 
-        inp = np.concatenate([lon_lat, dem, fc, fh, slope, easting, northing, twi])
-
+        inp = []
+        for f in self.features:
+            if f == "lon":
+                inp.append([self.lon_lat_ds[index, 0]])
+            elif f == "lat":
+                inp.append([self.lon_lat_ds[index, 1]])
+            elif f == "dem":
+                inp.append(self.dem_ds[index])
+            elif f == "fc":
+                inp.append(self.fc_ds[index])
+            elif f == "fh":
+                inp.append(self.fh_ds[index])
+            elif f == "slope":
+                inp.append(self.slope_ds[index])
+            elif f == "easting":
+                inp.append(self.easting_ds[index])
+            elif f == "northing":
+                inp.append(self.northing_ds[index])
+            elif f == "twi":
+                inp.append(self.twi_ds[index])
+            elif f == "rugg":
+                inp.append(self.rugg_ds[index])
+            elif f == "curv":
+                inp.append(self.curv_ds[index])
+        inp = np.concatenate(inp)
         return ndvi, doy, inp
 
     def __len__(self):
@@ -56,11 +88,11 @@ class H5Dataset():
 
 
 def double_logistic_function(t, params):
-    M, m, sos, mat_minus_sos, sen, eos_minus_sen = torch.split(params, 1, dim=1)
+    sos, mat_minus_sos, sen, eos_minus_sen, M, m = torch.split(params, 1, dim=1)
     mat_minus_sos = nn.functional.softplus(mat_minus_sos)
     eos_minus_sen = nn.functional.softplus(eos_minus_sen)
-    sigmoid_sos_mat = nn.functional.sigmoid(-2 * (2 * sos + mat_minus_sos - 2 * t) / (mat_minus_sos + 1e-8))
-    sigmoid_sen_eos = nn.functional.sigmoid(-2 * (2 * sen + eos_minus_sen - 2 * t) / (eos_minus_sen + 1e-8))
+    sigmoid_sos_mat = nn.functional.sigmoid(-2 * (2 * sos + mat_minus_sos - 2 * t) / (mat_minus_sos + 1e-10))
+    sigmoid_sen_eos = nn.functional.sigmoid(-2 * (2 * sen + eos_minus_sen - 2 * t) / (eos_minus_sen + 1e-10))
     return (M - m) * (sigmoid_sos_mat - sigmoid_sen_eos) + m
 
 
@@ -71,62 +103,69 @@ def objective_pinball(params, t, ndvi, nan_mask, alpha=0.5, weights=None):
     # we need to reweight the qunatiles to prevent a degenerate solution
     if weights is not None:
         loss = loss * weights.repeat(YEARS_IN_TRAIN).unsqueeze(0)
-    fil_losses = loss[~nan_mask]
-    if torch.any(torch.isnan(fil_losses)):
-        print("NaN in loss !")
-        print(fil_losses)
-        raise ValueError
-    return torch.mean(fil_losses)
+    return torch.mean(loss[~nan_mask])
 
 
-def train():
+def train(name, features=None):
     torch.manual_seed(1)
 
-    writer = SummaryWriter()
+    writer = SummaryWriter("runs/{}".format(name))
 
-    max_iterations = 250000
+    max_iterations = 500000
     lr = 0.001
-    lr_decay_rate = 0.1
+    lr_decay_rate = 0.01
     batch_size = 256
     device = 'cuda'
 
     # rescale target
-    NDVI_SCALE = 1.0
     T_SCALE = 1.0 / 365.0
 
+    print("Starting model with:")
+    print("name = {}".format(name))
+
     print("Loading dataset into RAM...")
-    ds = H5Dataset(PATH)
+    if isinstance(features, str):
+        features = features.split(",")
+    ds = H5Dataset(PATH, features)
     # load avg missingness as a function of DOY (73 entries in total)
     missingness = torch.from_numpy(ds.missingness).to(device)
-    # TODO: profiling to determine num_workers
     loader = DataLoader(ds, batch_size=batch_size, drop_last=True, shuffle=True, num_workers=32)
 
+    print("Using features: {}".format(ds.features))
+
     # rescale input (stats obtained from full dataset)
-    MEANS = torch.tensor([
-        8.34458,  # lon
-        46.32019,  # lat
-        1104.5027,  # dem
-        43.102547,  # fc
-        15.638395,  # fh
-        26.418936,  # slope
-        -0.0069493465,  # easting
-        0.062305786,  # northing
-        2.8649843,  # twi
-        ]).to(device).unsqueeze(0)
-    STDS = torch.tensor([
-        0.9852665,  # lon
-        0.61929077,  # lat
-        409.8894,  # dem
-        40.840992,  # fc
-        30.395235,  # fh
-        13.358144,  # slope
-        0.69276434,  # easting
-        0.7184338,  # northing
-        1.861385,  # twi
-        ]).to(device).unsqueeze(0)
+    MEANS = {
+        "lon": 8.34458,
+        "lat": 46.32019,
+        "dem": 1104.5027,
+        "fc": 43.102547,
+        "fh": 15.638395,
+        "slope": 26.418936,
+        "easting": -0.0069493465,
+        "northing": 0.062305786,
+        "twi": 2.8649843,
+        "rugg": 8.363732,
+        "curv": 6.1888146e-05,
+    }
+    STDS = {
+        "lon": 0.9852665,
+        "lat": 0.61929077,
+        "dem": 409.8894,
+        "fc": 40.840992,
+        "fh": 30.395235,
+        "slope": 13.358144,
+        "easting": 0.69276434,
+        "northing": 0.7184338,
+        "twi": 1.861385,
+        "rugg": 5.4581103,
+        "curv": 0.0061930786,
+    }
+
+    means_pt = torch.tensor([MEANS[f] for f in ds.features]).to(device).unsqueeze(0)
+    stds_pt = torch.tensor([STDS[f] for f in ds.features]).to(device).unsqueeze(0)
 
     # this model has ~470k parameters
-    encoder = MLP(d_in=9, d_out=12, n_blocks=8, d_block=256, dropout=0, skip_connection=True).to(device)
+    encoder = MLP(d_in=len(ds.features), d_out=8, n_blocks=8, d_block=256, dropout=0, skip_connection=True).to(device)
     # we don't care about overfitting --> no weight decay
     optimizer = torch.optim.Adam(encoder.parameters(), lr=lr)
 
@@ -151,46 +190,34 @@ def train():
             nan_mask = nan_mask | outlier_mask
 
             # separate into train/test
-            ndvi_train, ndvi_test = ndvi[:, :SPLIT_IDX], ndvi[:, SPLIT_IDX:]
-            doy_train, doy_test = doy[:, :SPLIT_IDX], doy[:, SPLIT_IDX:]
-            nan_mask_train, nan_mask_test = nan_mask[:, :SPLIT_IDX], nan_mask[:, SPLIT_IDX:]
+            ndvi_train = ndvi[:, :SPLIT_IDX]
+            doy_train = doy[:, :SPLIT_IDX]
+            nan_mask_train = nan_mask[:, :SPLIT_IDX]
 
-            # TODO: add positional encoding
+            inp = inp.float().to(device)
 
-            inp = inp.float().to(device)  # B x 9
             # standardize input
-            inp = (inp - MEANS) / STDS
+            inp = (inp - means_pt) / stds_pt
 
             # impute possible missing values with mean (should be the case only for fc)
             inp = torch.nan_to_num(inp, nan=0.0)
 
-            t_ndvi_train = ndvi_train.float().to(device) * NDVI_SCALE
+            t_ndvi_train = ndvi_train.float().to(device)
             t_doy_train = doy_train.float().to(device) * T_SCALE
             t_nan_mask_train = nan_mask_train.to(device)
 
-            # we predict a residual
-            preds = encoder(inp.float()) # B x 12
-            paramsl = preds[:, :6]  # B x 6
-            try:
-                lossl = objective_pinball(paramsl, t_doy_train, t_ndvi_train, t_nan_mask_train, alpha=0.25, weights=missingness)
-            except ValueError as e:
-                print("Error in iteration {}".format(n_iterations))
-                raise e
+            preds = encoder(inp.float()) # B x 8
+            paramsl = preds[:, [0, 1, 2, 3, 4, 5]]  # B x 6
+            paramsu = torch.cat([preds[:, [0, 1, 2, 3]], preds[:, [4, 5]] + nn.functional.softplus(preds[:, [6, 7]])], axis=1)
 
-            paramsu = preds[:, 6:]
-            try:
-                lossu = objective_pinball(paramsu, t_doy_train, t_ndvi_train, t_nan_mask_train, alpha=0.75, weights=missingness)
-            except ValueError as e:
-                print("Error in iteration {}".format(n_iterations))
-                raise e
-            
+            lossl = objective_pinball(paramsl, t_doy_train, t_ndvi_train, t_nan_mask_train, alpha=0.25, weights=missingness)
+            lossu = objective_pinball(paramsu, t_doy_train, t_ndvi_train, t_nan_mask_train, alpha=0.75, weights=missingness)
             loss = lossl + lossu
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            # update lr
             new_lrate = lr * (lr_decay_rate ** (n_iterations / max_iterations))
             for param_group in optimizer.param_groups:
                 param_group['lr'] = new_lrate
@@ -205,27 +232,23 @@ def train():
                     fig, ax = plt.subplots(2, 2, figsize=(15, 6), sharey=True)
 
                     t_fit = torch.linspace(0, 365, 1000).unsqueeze(0).repeat(paramsl.shape[0], 1)
-                    ndvi_lower = double_logistic_function(t_fit * T_SCALE, paramsl.cpu()) / NDVI_SCALE
-                    ndvi_upper = double_logistic_function(t_fit * T_SCALE, paramsu.cpu()) / NDVI_SCALE
+                    ndvi_lower = double_logistic_function(t_fit * T_SCALE, paramsl.cpu())
+                    ndvi_upper = double_logistic_function(t_fit * T_SCALE, paramsu.cpu())
 
                     random_indices = np.random.choice(np.arange(batch_size), size=4, replace=False)
                     for pl_idx, bi in enumerate(random_indices):
-
                         row, col = divmod(pl_idx, 2)
-
                         masked_ndvi_train = ndvi_train[bi][~nan_mask_train[bi]]
                         masked_doy_train = doy_train[bi][~nan_mask_train[bi]]
                         ax[row, col].scatter(masked_doy_train, masked_ndvi_train, label='Observed NDVI')
-
                         ax[row, col].fill_between(t_fit[bi], ndvi_lower[bi], ndvi_upper[bi], alpha=0.2, color='red')
                         ax[0, 0].set_ylabel("NDVI")
                         ax[1, 0].set_ylabel("NDVI")
 
                     writer.add_figure("Fit", fig, n_iterations)
 
-                    # TODO: computing scoring rule on val/test
-                    all_ndvi_lower = double_logistic_function(doy_train * T_SCALE, paramsl.cpu()) / NDVI_SCALE
-                    all_ndvi_upper = double_logistic_function(doy_train * T_SCALE, paramsu.cpu()) / NDVI_SCALE
+                    all_ndvi_lower = double_logistic_function(doy_train * T_SCALE, paramsl.cpu())
+                    all_ndvi_upper = double_logistic_function(doy_train * T_SCALE, paramsu.cpu())
                     all_masked_ndvi_lower = all_ndvi_lower[~nan_mask_train]
                     all_masked_ndvi_upper = all_ndvi_upper[~nan_mask_train]
                     all_masked_ndvi_train = ndvi_train[~nan_mask_train]
@@ -236,10 +259,9 @@ def train():
                     writer.add_scalar("D2PinballScoreUpper/train", d2_score_upper, n_iterations)
 
             if (n_iterations + 1) % 10000 == 0:
-                torch.save(encoder.state_dict(), "/data_1/scratch_1/dbrueggemann/nn/encoder_full.pt")
+                torch.save(encoder.state_dict(), "/data_1/scratch_1/dbrueggemann/nn/encoder_{}.pt".format(name))
 
             n_iterations += 1
-
             if n_iterations >= max_iterations:
                 stop = True
                 break
@@ -250,11 +272,15 @@ def train():
         n_epochs += 1
         writer.add_scalar("Epochs", n_epochs, n_iterations)
 
-    torch.save(encoder.state_dict(), "/data_1/scratch_1/dbrueggemann/nn/encoder_full.pt")
+    torch.save(encoder.state_dict(), "/data_1/scratch_1/dbrueggemann/nn/encoder_{}.pt".format(name))
 
     writer.flush()
     writer.close()
 
 
 if __name__ == "__main__":
-    train()
+    parser = argparse.ArgumentParser('train_double_logistic')
+    parser.add_argument('-n', '--name', type=str)
+    parser.add_argument('--features', type=str)
+    args = parser.parse_args()
+    train(args.name, args.features)
